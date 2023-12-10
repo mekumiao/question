990455339bb2cd2389637b89cd2e9d24d8d7f4a8
mekumiao <btguigushi@qq.com>
2023-12-10 18:07:22 +0800

    添加tailwind样式

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="flex flex-col">
+    <div class="flex flex-row justify-between">
+      <div>icon</div>
+      <nav class="flex flex-row">
+        <ul class="flex flex-row justify-start items-center text-xl font-bold">
+          <li class="px-1">开始考试</li>
+          <li class="px-1">模拟考试</li>
+          <li class="px-1">考试记录</li>
+          <li class="px-1">错题重做</li>
+          <li class="px-1">我要练习</li>
+        </ul>
+        <div
+          class="w-16 h-16 border rounded-full flex justify-center items-center bg-blue-700 text-white"
+        >
+          头像
+        </div>
+      </nav>
+    </div>
+    <div>xxx</div>
+  </div>
 </template>
+
+<style scoped></style>

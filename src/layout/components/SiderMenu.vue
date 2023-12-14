712c9ebaaf823a4be85e995b1943d4b7273a889b
mekumiao <btguigushi@qq.com>
2023-12-14 15:59:57 +0800

    侧边菜单刷新时自动激活

--- src/layout/components/SiderMenu.vue
+++ src/layout/components/SiderMenu.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { NMenu, NLayout, NLayoutSider } from 'naive-ui'
-import { h, ref, type Component } from 'vue'
+import { h, ref, type Component, onMounted } from 'vue'
 import { NIcon } from 'naive-ui'
 import type { MenuOption } from 'naive-ui'
 import {
@@ -13,7 +13,17 @@ import {
   TimeOutline,
 } from '@vicons/ionicons5'
 import { DashboardOutlined } from '@vicons/antd'
-import { RouterLink, type RouteLocationRaw } from 'vue-router'
+import { RouterLink, type RouteLocationRaw, useRoute } from 'vue-router'
+
+const collapsed = ref(false)
+const selectedKey = ref<string>()
+const menuInstRef = ref<InstanceType<typeof NMenu>>()
+const route = useRoute()
+
+onMounted(() => {
+  console.log(route.fullPath)
+  selectAndExpand(route.fullPath)
+})
 
 function renderMenuLabel(label: string, to?: RouteLocationRaw) {
   if (to) {
@@ -27,10 +37,15 @@ function renderIcon(icon: Component) {
   return () => h(NIcon, null, { default: () => h(icon) })
 }
 
+function selectAndExpand(key: string) {
+  selectedKey.value = key
+  menuInstRef.value?.showOption(key)
+}
+
 const menuOptions: MenuOption[] = [
   {
     label: renderMenuLabel('仪表板', { path: '/admin/dashboard' }),
-    key: 'dashboard',
+    key: '/admin/dashboard',
     icon: renderIcon(DashboardOutlined),
   },
   {
@@ -40,12 +55,12 @@ const menuOptions: MenuOption[] = [
     children: [
       {
         label: renderMenuLabel('用户列表', { path: '/admin/user' }),
-        key: 'user',
+        key: '/admin/user',
         icon: renderIcon(PeopleOutline),
       },
       {
         label: renderMenuLabel('学生列表', { path: '/admin/student' }),
-        key: 'student',
+        key: '/admin/student',
         icon: renderIcon(SchoolOutline),
       },
     ],
@@ -57,24 +72,22 @@ const menuOptions: MenuOption[] = [
     children: [
       {
         label: renderMenuLabel('题目列表', { path: '/admin/question' }),
-        key: 'question',
+        key: '/admin/question',
         icon: renderIcon(BookmarkOutline),
       },
       {
         label: renderMenuLabel('试卷列表', { path: '/admin/exam' }),
-        key: 'exam',
+        key: '/admin/exam',
         icon: renderIcon(RibbonOutline),
       },
       {
         label: renderMenuLabel('答题历史', { path: '/admin/history' }),
-        key: 'history',
+        key: '/admin/history',
         icon: renderIcon(TimeOutline),
       },
     ],
   },
 ]
-
-const collapsed = ref(false)
 </script>
 
 <template>
@@ -90,10 +103,13 @@ const collapsed = ref(false)
         @expand="collapsed = false"
       >
         <NMenu
+          ref="menuInstRef"
+          v-model:value="selectedKey"
           :collapsed="collapsed"
           :collapsed-width="64"
           :collapsed-icon-size="22"
           :options="menuOptions"
+          :accordion="true"
         />
       </NLayoutSider>
     </NLayout>

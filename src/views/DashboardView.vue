8df33f7d5c438adf5bc56695cd2784ed8bdaf120
mekumiao <btguigushi@qq.com>
2023-12-15 16:42:52 +0800

    添加dashboard

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,7 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { NStatistic, NIcon } from 'naive-ui'
+import { MdSave } from '@vicons/ionicons4'
+import { UserAddOutlined } from '@vicons/antd'
+</script>
 
 <template>
-  <div class="dashboard-view">dashboard</div>
+  <div class="dashboard-view grid grid-cols-4 grid-rows-2 gap-4">
+    <NStatistic
+      label="统计数据"
+      :value="99"
+      class="col-span-1 row-span-1 rounded border border-solid border-gray-300 p-2 shadow"
+    >
+      <template #prefix>
+        <NIcon>
+          <MdSave />
+        </NIcon>
+      </template>
+      <template #suffix> / 100 </template>
+    </NStatistic>
+    <NStatistic
+      label="活跃用户"
+      tabular-nums
+      class="col-span-1 row-span-1 rounded border border-solid border-gray-300 p-2 shadow"
+    >
+      <template #prefix>
+        <NIcon>
+          <UserAddOutlined />
+        </NIcon>
+      </template>
+      1,234,123
+    </NStatistic>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>

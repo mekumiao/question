3c21c52d53284082bf8ff57afa326aa931f0ae6a
mekumiao <btguigushi@qq.com>
2023-12-16 20:44:05 +0800

    优化404页面

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -4,7 +4,7 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <div class="not-found flex flex-row items-center justify-center">
+  <div class="not-found flex flex-row items-center justify-center h-screen">
     <NResult status="404" title="404 资源不存在" description="生活总归带点荒谬">
       <template #footer>
         <RouterLink to="/home"><NButton type="success">返回首页</NButton></RouterLink>
